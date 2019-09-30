e920ad850885f8059d1cd26ace7b9cc66dfe3495
rehmsa18 <rehmsa18@b048-09.crio.dauphine.fr>
2019-09-30 15:17:41 +0200

    first commit

---
+++ td00/src/main/java/fr/dauphine/ja/urrehmansania/td00/PrimeCollection.java
@@ -0,0 +1,47 @@
+package fr.dauphine.ja.urrehmansania.td00;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Hello world!
+ *
+ */
+public class PrimeCollection {
+	
+	private ArrayList <Integer> numbers;
+	
+	public PrimeCollection() {
+		numbers = new ArrayList <Integer>();
+	}
+	
+	public void initRandom(int n,int m) {
+		Random random = new Random();
+		for (int i = 0; i < n ; i++) {
+			numbers.add(random.nextInt(m) );
+		}	
+	}
+	
+	private boolean isPrime(int p) {	
+		for(int i = 2;i<= Math.sqrt(p) ;i++) { 
+			if (p%i==0) 
+				return false;
+		}
+		return true;	
+	}
+	
+	public void printPrimes() {
+		for (int i = 0; i < numbers.size() ; i++) {
+			if (isPrime(numbers.get(i)))
+				System.out.println(numbers.get(i));
+		}	
+		
+	}
+	
+	
+    public static void main( String[] args ){
+    	PrimeCollection pc = new PrimeCollection();
+    	pc.initRandom(100, 1000);
+    	pc.printPrimes();
+    }
+}
