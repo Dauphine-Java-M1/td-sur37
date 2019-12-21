6b44cf1d0fee9842bd3cc8151eb28cff87a5ba5a
Sania Ur Rehman <sania@prestachef.fr>
2019-12-21 18:12:39 +0100

    add readerwriters td

---
+++ readerwriters/src/main/java/fr/dauphine/ja/saniaurrehman/readerwriters/readerwriters/App.java
@@ -0,0 +1,13 @@
+package fr.dauphine.ja.saniaurrehman.readerwriters.readerwriters;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
